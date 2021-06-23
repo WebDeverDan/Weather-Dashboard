6e5ce3136331af2ed0ed61fd0271da1767a7028d
Daniel Bradley <danielbvideo@gmail.com>
2021-06-22 23:59:30 -0400

    This is the initial push of resources into online repository

---
+++ README.md
@@ -0,0 +1,13 @@
+This application is a simple password generator based on user input for number of characters, symbols, numbers, uppercase, and lowercase letters. The password can be a length of 8 - 128 characters as specified in the project requirements. 
+
+The interface is quite simple, although a few modifications have been made. Initially, it was not intuitive to click the Generate Password button, so instructions have been put into the placeholder box above the button. I have also changed the color of the button face to make it look a little more approachable. 
+
+The code was quite challenging, and the need to comment out most of the lines was imperative to understand how it works. The initial trigger is the Generate Password button, which calls a prompt of user inputting the desired length (between 8-128 characters). Once they have input their desired length, other prompts to include symbols, numbers, lowercase and uppercase letters. If none were selected, an alert appears reminding the user that atleast one needs to be answered. 
+
+Once the prompts complete, the password will generate based on the length and a random string of characters that is assembled from selected options. The password appears in the placeholder box upon completion. 
+
+![Image 1 of application:]()
+![Image 2 of application:]()
+ 
+Github Repository: 
+Github pages deployment: 
---
+++ script.js
@@ -0,0 +1,80 @@
+
+//Event Listener to prompt 'prompt' questions when element "#generate" button is pushed
+document.querySelector("#generate").addEventListener("click", writePassword);
+
+// Arrays of numbers, symbols, and characters used in password generation
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "<", ">","?"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Variables that are defined by user
+var confirmLength = "";
+var confirmSymbols;
+var confirmNumbers;
+var confirmUpperChar;
+var confirmLowerChar;
+
+// Prompt appears, asking users how many characters they would like in the password. This starts the prompt sequence and will roll through using "ok" to confirm and "cancel" to skip.
+function generatePassword() {
+  var confirmLength = (prompt("How many characters do you want your password to be?"));
+
+  // Loop back if answer is outside the parameters.
+  while(confirmLength <= 7 || confirmLength >= 129) {
+      alert("Password length must be between 8-128 characters. Please enter the correct length.");
+      var confirmLength = (prompt("How many characters would your password to be?"));
+      } 
+
+    // User will define the desired variables (symbols, numbers, upper and lowercase letters).
+    var confirmSymbols = confirm("Click OK if you want to include symbols in your password.");
+    var confirmNumbers = confirm("Click OK if you want to include numbers in your password.");    
+    var confirmLowerChar = confirm("Click OK if you want to to include lowercase letters in your password.");
+    var confirmUpperChar = confirm("Click OK if you want to include uppercase letters in your password.");
+      // If they opt out of all of the options, they must select atleast one.
+      while(confirmUpperChar === false && confirmLowerChar === false && confirmSymbols === false && confirmNumbers === false) {
+        alert("You must choose at least one of the previous options to continue.");
+        var confirmSymbols = confirm("Click OK to confirm that you want to include symbols.");
+        var confirmNumbers = confirm("Click OK to confirm that you want to include numbers.");    
+        var confirmLowerChar = confirm("Click OK to confirm that you want to include lowercase letters.");
+        var confirmUpperChar= confirm("Click OK to confirm that you want to include uppercase letters.");   
+    } 
+
+    // This section will assign actions to each if the criteria is selected
+      var passwordChars = []
+      
+    if (confirmSymbols) {
+      passwordChars = passwordChars.concat(symbols)
+    }
+
+    if (confirmNumbers) {
+      passwordChars = passwordChars.concat(numbers)
+    }
+      
+    if (confirmLowerChar) {
+      passwordChars = passwordChars.concat(lowerChar)
+    }
+
+    if (confirmUpperChar) {
+      passwordChars = passwordChars.concat(upperChar)
+    }
+
+      console.log(passwordChars)
+
+      //Password string is empty since the variable is to be filled based on parameters
+      var randomPassword = ""
+      // This will present list based based on input password length and randomized selected character types. Adds 1 to each until value ceiling (128) is reached (based on variable of 'confirmLength' on line 25)
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordChars[Math.floor(Math.random() * passwordChars.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
+}
+// End of generation.
+
+// Function to actually write and place password in "#password" element area.
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
