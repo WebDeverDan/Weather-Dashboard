b4508a5b09d6bfabde527bc701368a671d00687c
Daniel Bradley <danielbvideo@gmail.com>
2021-07-20 00:12:40 -0400

    update to screenshots, readme, and script

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,7 @@
 // when user clicks search, the city information will appear in the main box, and below the 5 day forecast will appear (done)
 // within the main box, UV index will appear, and the color of the index will change based on paramaters. (done)
 // the city will also appear as a clickable button on the left panel. This will occur each time a new city is searched (done)
-// upon clicking the city button, that city's information will re-appear
+// upon clicking the city button, that city's information will re-appear (done)
 // information will be stored and persist in local storage
 // make page look decent (done)
 
@@ -10,26 +10,38 @@
 // Date will pull in automatically using Moment
 var now = moment().format(" (MM/D/YYYY)");
 $("#currentDay").text(now);
-console.log(now);
-
-var submitButton = document.getElementById("searchBtn");
-var inputValue = document.getElementById("inputValue");
-var city = document.getElementById("city");
-var temp = document.getElementById("temp");
-var wind = document.getElementById("wind");
-var humidity = document.getElementById("humidity");
-var uv = document.getElementById("uv");
-var buttons = document.querySelector("buttons");
+// console.log(now);
 
 
-// function for pulling weather data based on the searched location. This will pull the 5 day forecast, uv index, change photos, etc., all based on the click event listener
-submitButton.addEventListener('click', function showcityinfo(){
+
+$(document).ready(function () {
+
+  var submitButton = document.getElementById("searchBtn");
+  var inputValue = document.getElementById("inputValue");
+  var city = document.getElementById("city");
+  var temp = document.getElementById("temp");
+  var wind = document.getElementById("wind");
+  var humidity = document.getElementById("humidity");
+  var uv = document.getElementById("uv");
+  var buttons = document.querySelector("buttons");
+  var history = [];
+
+  // localStorage.getItem("history", JSON.parse(history));
+  
+  
+  function showcityinfo(cityName) {
     document.getElementById("weatherTitleBox").style.display = "flex";
-    var mainAPI = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&units=imperial&appid=60e9ee923c45e087f45389019a259b46';
+    
+    // localStorage.getItem(JSON.parse([]));
+    var mainAPI =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&units=imperial&appid=60e9ee923c45e087f45389019a259b46";
+
     fetch(mainAPI)
-    .then(Response => Response.json())
-    // .then(data => console.log(data))
-    .then(data => {
+      .then((data) => data.json())
+      // .then(data => console.log(data))
+      .then((data) => {
         var cityValue = data["name"];
         var tempValue = data["main"]["temp"];
         var windValue = data["wind"]["speed"];
@@ -37,94 +49,271 @@ submitButton.addEventListener('click', function showcityinfo(){
         var latValue = data["coord"]["lat"];
         var longValue = data["coord"]["lon"];
 
-        // I will need to append the whole weather title with the date to the dom. 
-        $("#currentDayImage").attr("src",`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`) 
+        // I will need to append the whole weather title with the date to the dom.
+        $("#currentDayImage").attr(
+          "src",
+          `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+        );
 
         city.innerHTML = cityValue, " ";
         temp.innerHTML = tempValue;
         wind.innerHTML = windValue;
         humidity.innerHTML = humidityValue;
-     
-    //this is a sub fetch. it is the api that has the uv index but only by longitude and latitude. using the variables created prior, they were inserted into the api as the properties.
-        fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+latValue+'&lon='+longValue+'&appid=60e9ee923c45e087f45389019a259b46')
-        .then(Response => Response.json())
-        // .then(data => console.log(data))
-        .then(data => {
+
+        history.push(cityValue);
+        localStorage.setItem("history", JSON.stringify(history));
+        //this is a sub fetch. it is the api that has the uv index but only by longitude and latitude. using the variables created prior, they were inserted into the api as the properties.
+        fetch(
+          "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+            latValue +
+            "&lon=" +
+            longValue +
+            "&appid=60e9ee923c45e087f45389019a259b46"
+        )
+          .then((data) => data.json())
+          // .then(data => console.log(data))
+          .then((data) => {
             var uvValue = data["current"]["uvi"];
             uv.innerHTML = uvValue;
-            
+
             // code for changing color of uv index background based on the uv index
             // green
-            if(uvValue <= 1.99)  
-            $("#uv").css({"background-color":"#228B22","color":"#ffffff"});
+            if (uvValue <= 1.99)
+              $("#uv").css({ "background-color": "#228B22", color: "#ffffff" });
             // yellow
-            else if(uvValue >= 2 && uvValue<= 5.99)
-            $("#uv").css({"background-color":"#FFFF00","color":"#000000"});
+            else if (uvValue >= 2 && uvValue <= 5.99)
+              $("#uv").css({ "background-color": "#FFFF00", color: "#000000" });
             // orange
-            else if(uvValue >= 6 && uvValue<= 7.99)
-            $("#uv").css({"background-color":"#FFA500","color":"#000000"}); 
+            else if (uvValue >= 6 && uvValue <= 7.99)
+              $("#uv").css({ "background-color": "#FFA500", color: "#000000" });
             // red
-            else if(uvValue >= 8 && uvValue <= 9.99)
-            $("#uv").css({"background-color":"#FF0000","color":"#ffffff"});
+            else if (uvValue >= 8 && uvValue <= 9.99)
+              $("#uv").css({ "background-color": "#FF0000", color: "#ffffff" });
             // purple
-            else(
-            $("#uv").css({"background-color":"#800080","color":"#ffffff"}));
-        
-        // this creates buttons on the sidebar. eventually need to be able to click on them and write the data in the center div
+            else
+              $("#uv").css({ "background-color": "#800080", color: "#ffffff" });
+
+            // this creates buttons on the sidebar. eventually need to be able to click on them and write the data in the center div
             var buttonDiv = document.getElementById("buttonBox");
             var cityButton = document.createElement("button");
+
             cityButton.type = "button";
             cityButton.innerHTML = cityValue;
-            buttonDiv.appendChild(cityButton);  
-        }); 
-            
-    // this section is for the 5 day forecast
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q='+inputValue.value+'&units=imperial&results=10&appid=60e9ee923c45e087f45389019a259b46')
-    .then(Response => Response.json())
-    .then(data => {
-        // console.log(data)
-        console.log("forecast_is_here")
-        
-        console.log(data.list[3])
-        // this is the setup for each mini time block. the 3 refers to the day in list of objects found in json data., the time is in unix timecode, and the format is from moment
-        $("#day1Date").html(moment(data.list[3].dt,"X").format("MM/DD/YYYY"))
-        $("#day1Temp").html(data.list[3].main.temp)
-        $("#day1Image").attr("src",`http://openweathermap.org/img/wn/${data.list[11].weather[0].icon}@2x.png`).show()
-        $("#day1Humidity").html(data.list[3].main.humidity)
-        $("#day1Wind").html(data.list[3].wind.speed)
+            buttonDiv.appendChild(cityButton);
 
-        console.log(data.list[11])
-        $("#day2Date").html(moment(data.list[11].dt,"X").format("MM/DD/YYYY"))
-        $("#day2Temp").html(data.list[11].main.temp)
-        $("#day2Image").attr("src",`http://openweathermap.org/img/wn/${data.list[11].weather[0].icon}@2x.png`).show()
-        $("#day2Humidity").html(data.list[11].main.humidity)
-        $("#day2Wind").html(data.list[11].wind.speed)
+            // localStorage.setItem(uvValue, JSON.stringify(data));
 
-        console.log(data.list[19])
-        $("#day3Date").html(moment(data.list[19].dt,"X").format("MM/DD/YYYY"))
-        $("#day3Temp").html(data.list[19].main.temp)
-        $("#day3Image").attr("src",`http://openweathermap.org/img/wn/${data.list[19].weather[0].icon}@2x.png`).show()
-        $("#day3Humidity").html(data.list[19].main.humidity)
-        $("#day3Wind").html(data.list[19].wind.speed)
+            // this section is for the 5 day forecast
+            var forecastAPI =
+              "https://api.openweathermap.org/data/2.5/forecast?q=" +
+              cityName +
+              "&units=imperial&results=10&appid=60e9ee923c45e087f45389019a259b46";
+            fetch(forecastAPI)
+              .then((data) => data.json())
+              .then((data) => {
+                // console.log(data)
+                // console.log("forecast_is_here")
 
-        console.log(data.list[26])
-        $("#day4Date").html(moment(data.list[26].dt,"X").format("MM/DD/YYYY"))
-        $("#day4Temp").html(data.list[26].main.temp)
-        $("#day4Image").attr("src",`http://openweathermap.org/img/wn/${data.list[26].weather[0].icon}@2x.png`).show()
-        $("#day4Humidity").html(data.list[26].main.humidity)
-        $("#day4Wind").html(data.list[26].wind.speed)
+                // console.log(data.list[3])
+                // this is the setup for each mini time block. the 3 refers to the day in list of objects found in json data., the time is in unix timecode, and the format is from moment
+                $("#day1Date").html(
+                  moment(data.list[3].dt, "X").format("MM/DD/YYYY")
+                );
+                $("#day1Temp").html(data.list[3].main.temp);
+                $("#day1Image")
+                  .attr(
+                    "src",
+                    `http://openweathermap.org/img/wn/${data.list[11].weather[0].icon}@2x.png`
+                  )
+                  .show();
+                $("#day1Humidity").html(data.list[3].main.humidity);
+                $("#day1Wind").html(data.list[3].wind.speed);
 
-        console.log(data.list[34])
-        $("#day5Date").html(moment(data.list[34].dt,"X").format("MM/DD/YYYY"))
-        $("#day5Temp").html(data.list[34].main.temp)
-        $("#day5Image").attr("src",`http://openweathermap.org/img/wn/${data.list[34].weather[0].icon}@2x.png`).show()
-        $("#day5Humidity").html(data.list[34].main.humidity)
-        $("#day5Wind").html(data.list[34].wind.speed)
-        });
-    }); 
-});
-    
+                // console.log(data.list[11])
+                $("#day2Date").html(
+                  moment(data.list[11].dt, "X").format("MM/DD/YYYY")
+                );
+                $("#day2Temp").html(data.list[11].main.temp);
+                $("#day2Image")
+                  .attr(
+                    "src",
+                    `http://openweathermap.org/img/wn/${data.list[11].weather[0].icon}@2x.png`
+                  )
+                  .show();
+                $("#day2Humidity").html(data.list[11].main.humidity);
+                $("#day2Wind").html(data.list[11].wind.speed);
+
+                // console.log(data.list[19])
+                $("#day3Date").html(
+                  moment(data.list[19].dt, "X").format("MM/DD/YYYY")
+                );
+                $("#day3Temp").html(data.list[19].main.temp);
+                $("#day3Image")
+                  .attr(
+                    "src",
+                    `http://openweathermap.org/img/wn/${data.list[19].weather[0].icon}@2x.png`
+                  )
+                  .show();
+                $("#day3Humidity").html(data.list[19].main.humidity);
+                $("#day3Wind").html(data.list[19].wind.speed);
+
+                // console.log(data.list[26])
+                $("#day4Date").html(
+                  moment(data.list[26].dt, "X").format("MM/DD/YYYY")
+                );
+                $("#day4Temp").html(data.list[26].main.temp);
+                $("#day4Image")
+                  .attr(
+                    "src",
+                    `http://openweathermap.org/img/wn/${data.list[26].weather[0].icon}@2x.png`
+                  )
+                  .show();
+                $("#day4Humidity").html(data.list[26].main.humidity);
+                $("#day4Wind").html(data.list[26].wind.speed);
+
+                // console.log(data.list[34])
+                $("#day5Date").html(
+                  moment(data.list[34].dt, "X").format("MM/DD/YYYY")
+                );
+                $("#day5Temp").html(data.list[34].main.temp);
+                $("#day5Image")
+                  .attr(
+                    "src",
+                    `http://openweathermap.org/img/wn/${data.list[34].weather[0].icon}@2x.png`
+                  )
+                  .show();
+                $("#day5Humidity").html(data.list[34].main.humidity);
+                $("#day5Wind").html(data.list[34].wind.speed);
+                
+                cityButton.addEventListener("click", function changeCityInfo() {
+                  city.innerHTML = cityValue;
+                  temp.innerHTML = tempValue;
+                  wind.innerHTML = windValue;
+                  humidity.innerHTML = humidityValue;
+                  uv.innerHTML = uvValue;
+
+                  if (uvValue <= 1.99)
+                    $("#uv").css({
+                      "background-color": "#228B22",
+                      color: "#ffffff",
+                    });
+                  // yellow
+                  else if (uvValue >= 2 && uvValue <= 5.99)
+                    $("#uv").css({
+                      "background-color": "#FFFF00",
+                      color: "#000000",
+                    });
+                  // orange
+                  else if (uvValue >= 6 && uvValue <= 7.99)
+                    $("#uv").css({
+                      "background-color": "#FFA500",
+                      color: "#000000",
+                    });
+                  // red
+                  else if (uvValue >= 8 && uvValue <= 9.99)
+                    $("#uv").css({
+                      "background-color": "#FF0000",
+                      color: "#ffffff",
+                    });
+                  // purple
+                  else
+                    $("#uv").css({
+                      "background-color": "#800080",
+                      color: "#ffffff",
+                    });
+
+                  fetch(mainAPI)
+                    .then((data) => data.json())
+                    .then((data) => {
+                      $("#currentDayImage").attr(
+                        "src",
+                        `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+                      );
+                    });
+
+                  fetch(forecastAPI)
+                    .then((data) => data.json())
+                    .then((data) => {
+                      // this is the setup for each mini time block. the 3 refers to the day in list of objects found in json data., the time is in unix timecode, and the format is from moment
+                      $("#day1Date").html(
+                        moment(data.list[3].dt, "X").format("MM/DD/YYYY")
+                      );
+                      $("#day1Temp").html(data.list[3].main.temp);
+                      $("#day1Image")
+                        .attr(
+                          "src",
+                          `http://openweathermap.org/img/wn/${data.list[11].weather[0].icon}@2x.png`
+                        )
+                        .show();
+                      $("#day1Humidity").html(data.list[3].main.humidity);
+                      $("#day1Wind").html(data.list[3].wind.speed);
+
+                      $("#day2Date").html(
+                        moment(data.list[11].dt, "X").format("MM/DD/YYYY")
+                      );
+                      $("#day2Temp").html(data.list[11].main.temp);
+                      $("#day2Image")
+                        .attr(
+                          "src",
+                          `http://openweathermap.org/img/wn/${data.list[11].weather[0].icon}@2x.png`
+                        )
+                        .show();
+                      $("#day2Humidity").html(data.list[11].main.humidity);
+                      $("#day2Wind").html(data.list[11].wind.speed);
+
+                      $("#day3Date").html(
+                        moment(data.list[19].dt, "X").format("MM/DD/YYYY")
+                      );
+                      $("#day3Temp").html(data.list[19].main.temp);
+                      $("#day3Image")
+                        .attr(
+                          "src",
+                          `http://openweathermap.org/img/wn/${data.list[19].weather[0].icon}@2x.png`
+                        )
+                        .show();
+                      $("#day3Humidity").html(data.list[19].main.humidity);
+                      $("#day3Wind").html(data.list[19].wind.speed);
+
+                      $("#day4Date").html(
+                        moment(data.list[26].dt, "X").format("MM/DD/YYYY")
+                      );
+                      $("#day4Temp").html(data.list[26].main.temp);
+                      $("#day4Image")
+                        .attr(
+                          "src",
+                          `http://openweathermap.org/img/wn/${data.list[26].weather[0].icon}@2x.png`
+                        )
+                        .show();
+                      $("#day4Humidity").html(data.list[26].main.humidity);
+                      $("#day4Wind").html(data.list[26].wind.speed);
+
+                      $("#day5Date").html(
+                        moment(data.list[34].dt, "X").format("MM/DD/YYYY")
+                      );
+                      $("#day5Temp").html(data.list[34].main.temp);
+                      $("#day5Image")
+                        .attr(
+                          "src",
+                          `http://openweathermap.org/img/wn/${data.list[34].weather[0].icon}@2x.png`
+                        )
+                        .show();
+                      $("#day5Humidity").html(data.list[34].main.humidity);
+                      $("#day5Wind").html(data.list[34].wind.speed);
+                    });
+                  // end of changeCityInfo function
+                });
+              });
+          });
+      });
+    }
+  
+  // showcityinfo(cityValue);
+  // function for pulling weather data based on the searched location. This will pull the 5 day forecast, uv index, change photos, etc., all based on the click event listener
+  submitButton.addEventListener("click", function () {
+    showcityinfo(inputValue.value);
+      });
+    });
 
 
 
-console.log("it's working")
+
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Weather-Dashboard
+The interface of this application features a search bar, and following user input results in unique weather results as Temperature, Wind Speed, Humidity, and UV Index. The UV index color changes depending on the level of the index at the time of the search. In addition to the individual results, a 5 day forecast appears at the bottom of the app. 
+
+Upon searching for a location, that location will be saved to the left-hand side of the page. To return to a previous and its information, the user can re-click on the respective button on the left and the information will re-populate. 
+
+**At the time of submission, the information is written to local storage, however I am still having difficulty retrieving from local storage**
+
+Link to Github Repository: 
+Link to Deployed application on Github pages:
+
+![Image 1 of application:]()
+![Image 2 of application:]()
+![Image 3 of application:]()
+![Image 4 of application:]()
